171249bee16a7b1a4bdfded1df7a86932cafbaaa
ChallenAi <fengyiai@gmail.com>
2022-07-27 23:35:40 +0800

    feat: add 2 example types for mongodb

---
+++ pkg/store/mongodb/defs.go
@@ -0,0 +1,10 @@
+package mongodb
+
+import "github.com/challenai/conveyer/pkg/transform"
+
+// https://www.mongodb.com/docs/manual/reference/bson-types/
+
+const (
+	TypeDouble transform.Type = "double"
+	TypeString transform.Type = "string"
+)
